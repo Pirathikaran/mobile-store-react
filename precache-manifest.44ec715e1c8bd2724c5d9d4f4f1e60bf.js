9d1e4a80e4a417a534aa2bfc28f59d4068a6bb6c
Pirathikaran <vpirathikaran18@gmail.com>
2020-03-31 22:08:43 +0530

    Updates

---
+++ precache-manifest.44ec715e1c8bd2724c5d9d4f4f1e60bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "488498a138ddc38c97cf9f5990e3a9e3",
+    "url": "/mobile-store-react/index.html"
+  },
+  {
+    "revision": "fecae95a807fc89ab6f1",
+    "url": "/mobile-store-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e8318b78026b13c95e4e",
+    "url": "/mobile-store-react/static/js/2.fcea253e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mobile-store-react/static/js/2.fcea253e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fecae95a807fc89ab6f1",
+    "url": "/mobile-store-react/static/js/main.3ec6bfc4.chunk.js"
+  },
+  {
+    "revision": "579737c961afdd0ebc50",
+    "url": "/mobile-store-react/static/js/runtime-main.6f7c3ca2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mobile-store-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
